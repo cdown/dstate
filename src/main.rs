17f863e7e41ab7914d2658d3b92ff271b231a0f0
Chris Down <chris@chrisdown.name>
2018-10-03 10:11:13 -0700

    Move macros to their own file

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,8 @@ use std::{str, string};
 
 static BUF_SIZE: usize = 1024 * 64; // a "large enough" buffer to do one read() on {proc,sys,kern}fs
 
-macro_rules! cont_on_err {
-    ($res:expr) => {
-        match $res {
-            Ok(val) => val,
-            Err(_) => continue,
-        }
-    };
-}
-
-macro_rules! cont_on_none {
-    ($res:expr) => {
-        match $res {
-            Some(val) => val,
-            None => continue,
-        }
-    };
-}
+#[macro_use]
+mod macros;
 
 #[derive(Debug)]
 enum DStateError {
